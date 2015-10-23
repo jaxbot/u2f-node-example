b2e8f981cf0247af3daf59265b7a53a4193436f7
Jonathan Warner <jaxbot@gmail.com>
2015-10-22 22:50:12 -0400

    Clean up

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ var engines = require('consolidate');
 var app = express();
 var https = require('https');
 var fs = require('fs');
-
 var u2f = require('u2f');
 
 var APP_ID = "https://localhost:4433";
+
+var SECRET_DATA = require("./secret_data.json");
 
 app.use(bodyParser.json());
 app.use(require('cookie-parser')());
@@ -22,25 +23,25 @@ app.get('/', function(req, res) {
   }
   res.render('index.html');
 });
-app.get('/register', function(req, res) {
-  res.render('register.html');
-});
-app.get('/secretstuff', function(req, res) {
-  res.render('secret.html');
-});
-app.get('/api/register_token', function(req, res) {
+
+app.get('/api/register_request', function(req, res) {
   var authRequest = u2f.request(APP_ID);
   Sessions[req.cookies.userid] = { authRequest: authRequest };
   res.send(JSON.stringify(authRequest));
 });
-app.get('/api/sign_token', function(req, res) {
+
+app.get('/api/sign_request', function(req, res) {
   var authRequest = u2f.request(APP_ID, Users[req.cookies.userid].keyHandle);
   Sessions[req.cookies.userid] = { authRequest: authRequest };
   res.send(JSON.stringify(authRequest));
 });
+
 app.post('/api/register', function(req, res) {
   console.log(req.body);
-  var checkRes = u2f.checkRegistration(Sessions[req.cookies.userid].authRequest, req.body);
+  var checkRes = u2f.checkRegistration(
+    Sessions[req.cookies.userid].authRequest,
+    req.body
+  );
   console.log(checkRes);
   if (checkRes.successful) {
     Users[req.cookies.userid] = { publicKey: checkRes.publicKey, keyHandle: checkRes.keyHandle };
@@ -51,19 +52,22 @@ app.post('/api/register', function(req, res) {
 });
 app.post('/api/authenticate', function(req, res) {
   console.log(req.body);
-  var checkRes = u2f.checkSignature(Sessions[req.cookies.userid].authRequest, req.body, Users[req.cookies.userid].publicKey);
+  var checkRes = u2f.checkSignature(
+    Sessions[req.cookies.userid].authRequest,
+    req.body,
+    Users[req.cookies.userid].publicKey
+  );
   console.log(checkRes);
   if (checkRes.successful) {
-    res.send("The secret data is: " + Math.random());
+    res.send({ success: true, secretData: SECRET_DATA });
   } else {
-    res.send(checkRes.errorMessage);
+    res.send({ error: checkRes.errorMessage });
   }
 });
 
-app.listen(5000);
-var privateKey = fs.readFileSync('../wiistuff/server.key');
-var certificate = fs.readFileSync('../wiistuff/server.crt');
-
-var credentials = {key: privateKey, cert: certificate};
+var credentials = {
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.crt')
+};
 var httpsServer = https.createServer(credentials, app);
 httpsServer.listen(4433);
